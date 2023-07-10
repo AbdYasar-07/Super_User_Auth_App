86f271b5a65ca38ee73fc0c3558838f68ab848c8
AbdYasar-07 <staryasar07@gmail.com>
2023-07-11 00:30:33 +0530

    scaffolding Initial Commit
    Initial Code Setup & Dependencies Installation Completed

---
+++ super_user_auth_app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import {FaBeer} from 'react-icons/fa';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="btn btn-primary m-5">Hello App <FaBeer /></button>
+    </div>
+  );
+}
+
+export default App;
